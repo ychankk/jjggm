f52c9a1b086323cd1cb7d0e5982f3b35c3c38ecf
ychankk <147124482+ychankk@users.noreply.github.com>
2023-10-19 11:14:08 +0900

    초기 커밋.

---
+++ jjggm/canvas.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include "jjuggumi.h"
+#include "canvas.h"
+
+#define DIALOG_DURATION_SEC		4
+
+void draw(void);
+void print_status(void);
+
+// (zero-base) row행, col열로 커서 이동
+void gotoxy(int row, int col) {
+	COORD pos = { col,row };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+// row행, col열에 ch 출력
+void printxy(char ch, int row, int col) {
+	gotoxy(row, col);
+	printf("%c", ch);
+}
+
+void map_init(int n_row, int n_col) {
+	// 두 버퍼를를 완전히 비우기
+	for (int i = 0; i < ROW_MAX; i++) {
+		for (int j = 0; j < COL_MAX; j++) {
+			back_buf[i][j] = front_buf[i][j] = ' ';
+		}
+	}
+
+	N_ROW = n_row;
+	N_COL = n_col;
+	for (int i = 0; i < N_ROW; i++) {
+		// 대입문 이렇게 쓸 수 있는데 일부러 안 가르쳐줬음
+		back_buf[i][0] = back_buf[i][N_COL - 1] = '#';
+
+		for (int j = 1; j < N_COL - 1; j++) {
+			back_buf[i][j] = (i == 0 || i == N_ROW - 1) ? '#' : ' ';
+		}
+	}
+}
+
+// back_buf[row][col]이 이동할 수 있는 자리인지 확인하는 함수
+bool placable(int row, int col) {
+	if (row < 0 || row >= N_ROW ||
+		col < 0 || col >= N_COL ||
+		back_buf[row][col] != ' ') {
+		return false;
+	}
+	return true;
+}
+
+// 상단에 맵을, 하단에는 현재 상태를 출력
+void display(void) {
+	draw();
+	gotoxy(N_ROW + 4, 0);  // 추가로 표시할 정보가 있으면 맵과 상태창 사이의 빈 공간에 출력
+	print_status();
+}
+
+void draw(void) {
+	for (int row = 0; row < N_ROW; row++) {
+		for (int col = 0; col < N_COL; col++) {
+			if (front_buf[row][col] != back_buf[row][col]) {
+				front_buf[row][col] = back_buf[row][col];
+				printxy(front_buf[row][col], row, col);
+			}
+		}
+	}
+}
+
+void print_status(void) {
+	printf("no. of players left: %d\n", n_alive);
+	for (int p = 0; p < n_player; p++) {
+		printf("player %2d: %5s\n", p, player[p] ? "alive" : "DEAD");
+	}
+}
+
+void dialog(char message[]) {
+
+}
---
+++ jjggm/jjuggumi.c
@@ -0,0 +1,39 @@
+// 2023-2 고급프로그래밍 과제: 쭈꾸미 게임
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include "jjuggumi.h"
+
+#define	DATA_FILE	"jjuggumi.dat"
+
+int jjuggumi_init(void);
+
+// low 이상 high 이하 난수를 발생시키는 함수
+int randint(int low, int high) {
+	int rnum = rand() % (high - low + 1) + low;
+	return rnum;
+}
+
+int jjuggumi_init(void) {
+	srand((unsigned int)time(NULL));
+
+	printf("플레이어 수: ");
+	scanf_s("%d", &n_player);
+
+	n_alive = n_player;
+	for (int i = 0; i < n_player; i++) {
+		player[i] = true;
+	}
+	return 0;
+}
+
+int main(void) {
+	jjuggumi_init();
+	sample();
+	//mugunghwa();
+	//nightgame();
+	//juldarigi();
+	//jebi();
+	return 0;
+}
